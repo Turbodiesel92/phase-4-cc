d9635fd7bf26d13647e8ae4fb5fc21e36e3ca244
MatthewWilliams <turbodieseldodge@gmail.com>
2023-03-23 10:45:16 -0500

    Code Challenge

--- server/app.py
+++ server/app.py
@@ -21,10 +21,56 @@ api = Api(app)
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
-def restaurants():
 
-    pass
+class Pizzas(Resource):
+    def get(self):
+
+        pizza_dicts = [pizza.to_dict() for pizza in Pizza.query.all()]
+
+        return make_response(
+            jsonify(pizza_dicts),
+            200
+        )
+
+api.add_resource(Pizzas, '/pizzas')
+
+class Restaurants(Resource):
+    def get(self):
+
+        restaurant_dicts = [restaurant.to_dict() for restaurant in Restaurant.query.all()]
+
+        return make_response(
+            jsonify(restaurant_dicts),
+            200
+        )
+
+api.add_resource(Restaurants, '/restaurants')
+
+
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+
+        if not restaurant:
+            return make_response({'error': "Restaurant not found"},404)
+
+        restaurant_dict = restaurant.to_dict()
+
+        return make_response(
+            jsonify(restaurant_dict),
+            200
+        )
+
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+
+
+# class RestaurantPizza(Resource):
+#     def post(self):
+#         try:
+#             new_restaurant_pizza = RestaurantPizza(
+
+#             )
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
